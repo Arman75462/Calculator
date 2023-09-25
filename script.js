66ce9a50d7240a85333bd7d226c9c4c86ed48314
Arman75462 <armangulistani@gmail.com>
2023-09-24 21:12:19 -0400

    Updated CSS colors/layout, added display container in screen.
    Layout/colors is more visually appealing and easier to navigate, the numbers dont overflow out of the screen container with the new code added.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /* ==========================
 =============Selectors============= 
 ==========================*/
-const screen = document.querySelector('.screen');
+const display = document.querySelector('.display');
 const plus = document.querySelector('.plus');
 const minus = document.querySelector('.minus');
 const multiplication = document.querySelector('.multiplication');
@@ -36,45 +36,48 @@ let operation = [];
 =============BUTTONS============= 
 ==========================*/
 plus.addEventListener('click', function() {
-    screen.textContent = '+';
+    display.textContent = '+';
     operation.push('+');
 })
 
 minus.addEventListener('click', function() {
-    screen.textContent = '-';
+    display.textContent = '-';
     operation.push('-');
 })
 
 multiplication.addEventListener('click', function() {
-    screen.textContent = 'x';
+    display.textContent = 'x';
     operation.push('*');
 })
 
 division.addEventListener('click', function() {
-    screen.textContent = '÷';
+    display.textContent = '÷';
     operation.push('/');
 })
 
 equal.addEventListener('click', function() {
     let result = eval(operation.join(''));
     operation = [];
-    screen.textContent = result;
+    display.textContent = result;
     console.log(result);
 })
 
 clear.addEventListener('click', function() {
-    screen.textContent = '0';
+    display.textContent = '0';
     operation = [];
 });
 
 deleteButton.addEventListener('click', function(){
     operation.pop();
-    screen.textContent = '';
+    display.textContent = operation[operation.length - 1];
+    if(display.textContent === '') {
+        display.textContent = '0';
+    } 
 })
 
 dot.addEventListener('click', function() {
     operation.push('.');
-    screen.textContent = '.'
+    display.textContent = '.'
 })
 
 
@@ -82,76 +85,62 @@ dot.addEventListener('click', function() {
 =============NUMBERS============= 
 ==========================*/
 zero.addEventListener('click', function() {
-    screen.textContent = 0;
-    operation.push('0');
+    display.textContent =  '0';
+    operation.push(0);
     console.log(operation);
-})
+});
 
 one.addEventListener('click', function() {
-    screen.textContent = 1;
-    operation.push('1');
+    display.textContent =  '1';
+    operation.push(1);
     console.log(operation);
-})
-
+});
 
 two.addEventListener('click', function() {
-    screen.textContent = 2;
-    operation.push('2');
+    display.textContent =  '2';
+    operation.push(2);
     console.log(operation);
-})
+});
 
 three.addEventListener('click', function() {
-    screen.textContent = 3;
-    operation.push('3');
+    display.textContent =  '3';
+    operation.push(3);
     console.log(operation);
-})
+});
 
 four.addEventListener('click', function() {
-    screen.textContent = 4;
-    operation.push('4');
+    display.textContent =  '4';
+    operation.push(4);
     console.log(operation);
-})
+});
 
 five.addEventListener('click', function() {
-    screen.textContent = 5;
-    operation.push('5');
+    display.textContent =  '5'
+    operation.push(5);
     console.log(operation);
-})
+});
 
 six.addEventListener('click', function() {
-    screen.textContent = 6;
-    operation.push('6');
+    display.textContent =  '6';
+    operation.push(6);
     console.log(operation);
-})
-
+});
 seven.addEventListener('click', function() {
-    screen.textContent = 7;
-    operation.push('7');
+    display.textContent =  '7';
+    operation.push(7);
     console.log(operation);
-})
+});
 
 eight.addEventListener('click', function() {
-    screen.textContent = 8;
-    operation.push('8');
+    display.textContent =  '8';
+    operation.push(8);
     console.log(operation);
-})
+});
 
 nine.addEventListener('click', function() {
-    screen.textContent = 9;
-    operation.push('9');
+    display.textContent =  '9';
+    operation.push(9);
     console.log(operation);
-})
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
